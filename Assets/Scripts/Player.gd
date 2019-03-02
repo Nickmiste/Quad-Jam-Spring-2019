d32b08a28cf3a37681fe210a83be3472558cd36b
Bowman J Dylan <bowma127@canton.edu>
2019-03-01 23:54:53 -0500

    Jumping Updates

--- Assets/Scripts/Player.gd
+++ Assets/Scripts/Player.gd
@@ -3,18 +3,22 @@ extends KinematicBody
 enum {IDLE, RUN, JUMP, HURT, DEAD}
 var state = IDLE
 
-#Global Constants
-const GRAVITY = 0.5
-const SPEED_RUN = 10
-const JUMP_MAX = 1
+#Global Constant Variables
+const GRAVITY = 6
 
-var move_speed = SPEED_RUN
-var jump_force = 25
+#Jump Variables
+const JUMP_FORCE = 9
+const JUMP_MAX = 1
 var jump_count = 1
+
+#Horizontal Movement Variables
+const SPEED_RUN = 10
+var move_speed = SPEED_RUN
 var velocity = Vector3(0,0,0)
 
 export (NodePath) var level_root
 
+#This function handles changing states
 func change_state(new_state):
 	state = new_state
 	match state:
@@ -36,11 +40,14 @@ func change_state(new_state):
 
 func _physics_process(delta):
 	velocity.y -= GRAVITY * delta
+	if velocity.y < 0:
+		velocity.y -= 8 * delta
 	velocity.x = 0
 	
 	control_process()
 	
 	move_and_slide(velocity, Vector3(0,1,0))
+	
 	if is_on_floor():
 		jump_count = JUMP_MAX
 
@@ -56,11 +63,13 @@ func control_process():
 	
 	if jump and jump_count > 0:
 		jump_count -= 1
-		velocity.y += jump_force
+		velocity.y += JUMP_FORCE
 	
+	#Switches state between Run and Idle
 	if velocity.x != 0:
 		change_state(RUN)
 	if velocity == Vector3(0,0,0):
 		change_state(IDLE)
+	
 
 
